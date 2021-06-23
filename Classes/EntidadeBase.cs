23a9b774ee4a60f4b07f95ac5edd2738a8c3e04c
PH <phpcareli@gmail.com>
2021-06-23 00:46:25 -0300

    First commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace DIO.Series
+{
+    public class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ Classes/Serie.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        // Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = Id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dot-net/api/system.environment.new
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Tíulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
